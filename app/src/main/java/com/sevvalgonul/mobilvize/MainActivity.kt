faa9ac099259d6f0331ccf9013b8b55a9dafe911
Şevval Gönül <sevvalgnl17@gmail.com>
2022-12-09 16:10:01 +0300

    Initial commit

---
+++ app/src/main/java/com/sevvalgonul/mobilvize/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.sevvalgonul.mobilvize
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.sevvalgonul.mobilvize.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navController = findNavController(R.id.fragmentContainerView)
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+
+    }
+}
