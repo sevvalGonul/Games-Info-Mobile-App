02b1f84362321ca5e46da2495a3caea479500027
Sinem Akyüz <sinemakyz57@gmail.com>
2022-12-10 20:31:23 +0300

    details fully completed (ui wise)

--- app/src/main/java/com/sevvalgonul/mobilvize/DetailsFragment.kt
+++ app/src/main/java/com/sevvalgonul/mobilvize/DetailsFragment.kt
@@ -1,6 +1,7 @@
 package com.sevvalgonul.mobilvize
 
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,11 +24,6 @@ class DetailsFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
