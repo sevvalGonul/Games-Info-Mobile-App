4e5a7affcad8c8ed6576dd9690e98d3f33381134
winused <nlytsl13@gmail.com>
2022-12-10 15:39:30 +0300

    Update MainActivity.kt

--- app/src/main/java/com/sevvalgonul/mobilvize/MainActivity.kt
+++ app/src/main/java/com/sevvalgonul/mobilvize/MainActivity.kt
@@ -18,6 +18,6 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.fragmentContainerView)
         binding.bottomNavigationView.setupWithNavController(navController)
 
-
+        print("Nilay")
     }
 }
