6e452cb541ca1c28c442acc6f7fd18dfcbad5f89
Şevval Gönül <sevvalgnl17@gmail.com>
2022-12-10 15:39:13 +0300

    deneme

--- app/src/main/java/com/sevvalgonul/mobilvize/MainActivity.kt
+++ app/src/main/java/com/sevvalgonul/mobilvize/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.fragmentContainerView)
         binding.bottomNavigationView.setupWithNavController(navController)
 
+        print("Şevval")
+
 
     }
 }
