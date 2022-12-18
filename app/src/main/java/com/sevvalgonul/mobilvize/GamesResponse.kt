a991ef1fa52f804905dd4363193004e8dc808750
Şevval Gönül <sevvalgnl17@gmail.com>
2022-12-18 23:10:41 +0300

    Some Kotlin Model Classes for API Response are added
    It's not include all model classes. Just for GamesFragment without genres

---
+++ app/src/main/java/com/sevvalgonul/mobilvize/GamesResponse.kt
@@ -0,0 +1,9 @@
+package com.sevvalgonul.mobilvize
+
+// A class that represents kotlin objects for the response of the API we use
+data class GamesResponse(
+    val count: Int,
+    val next: String,
+    val previous: String,
+    val results: List<Result>
+)
---
+++ app/src/main/java/com/sevvalgonul/mobilvize/Result.kt
@@ -0,0 +1,24 @@
+package com.sevvalgonul.mobilvize
+
+
+data class Result(  // Each game in the GameResponse, notice that it doesn't contain genres
+    //val added: Int,
+    //val added_by_status: AddedByStatus,
+    val background_image: String,
+    //val esrb_rating: EsrbRating,
+    val id: Int,
+    val metacritic: Int,
+    val name: String,
+    //val platforms: List<Platform>,
+    //val playtime: Int,
+    //val rating: Int,
+    //val rating_top: Int,
+    //val ratings: Ratings,
+    //val ratings_count: Int,
+    //val released: String,
+    //val reviews_text_count: String,
+    //val slug: String,
+    //val suggestions_count: Int,
+    //val tba: Boolean,
+    //val updated: String
+)
